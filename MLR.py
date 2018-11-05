96df4f85c13efa2f02fb25c259d0c08ee227ffaf
Ryan Boldi <Ryan.boldi123@gmail.com>
2018-11-05 19:35:38 +0400

    Added main Code base
    Also added a dataset that i used when testing

---
+++ MLR.py
@@ -0,0 +1,32 @@
+#mulitple linear regression
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+##importing dataset
+#file = "Haberman.csv"
+#dataset = pf.read_csv(file)
+#x = dataset.iloc[:, :-1].values
+#y = dataset.iloc[:,3].values
+
+from sklearn import load_boston
+x,y = load_boston(True)
+
+#encoding the categorical data
+from sklearn.preprocessing import LabelEncoder
+#encoding the dependant variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#splitting dataset into the training and testing datasets
+from sklearn.cross_validation import train_test_split
+X_train , X_test, Y_train, Y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
+
+#fitting multiple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+#predicting the y values for the x test values using the regressor
+y_predicted = regressor.predict(X_test)
