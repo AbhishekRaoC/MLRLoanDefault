76c1f9db559fc899f487bea905983e858cbe3c06
Ryan Boldi <ryan.boldi123@gmail.com>
2018-11-06 11:34:37 +0400

    Update MLR.py

--- MLR.py
+++ MLR.py
@@ -21,7 +21,7 @@ y = labelencoder_y.fit_transform(y)
 
 #splitting dataset into the training and testing datasets
 from sklearn.cross_validation import train_test_split
-X_train , X_test, Y_train, Y_test = train_test_split(x,y, test_size = 0.1, random_state = 0)
+X_train , X_test, Y_train, Y_test = train_test_split(x,y, test_size = 0.33, random_state = 0)
 
 #fitting multiple linear regression to the training set
 from sklearn.linear_model import LinearRegression
@@ -30,3 +30,5 @@ regressor.fit(X_train, Y_train)
 
 #predicting the y values for the x test values using the regressor
 y_predicted = regressor.predict(X_test)
+#test = [0 for i in range(13)]
+#test_pred = regressor.predict(np.array(test).reshape(1,-1))
